c45b4662325e2c4326ddab460ffbe0f88691aa46
Uwabor, Collins <legendarycolins@yahoo.com>
2025-09-23 19:29:39 +0100

    Updated

--- 01_Simple Part (100+ Questions)/test.py
+++ 01_Simple Part (100+ Questions)/test.py
@@ -1,11 +1,2 @@
-result = []
 
-for i in range(1, 31):
-    listSquares = i * i
-    if listSquares >= 30:
-        break
-    result.append(listSquares)
-print(result)
-print("The first 5 elements are: ", result[:5])
-print("The Last 5 elements are: ", result[-5:])
 
